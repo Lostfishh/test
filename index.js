a4e50c5cba72aa058ec8a695cc737018becf688f
bujiajia <bujiajia@sinodata.com.cn>
2021-06-30 02:07:41 +0800

    1

--- index.js
+++ index.js
@@ -57,11 +57,25 @@ var b = new Promise((resolve, reject) => {
   //     }
   //   }, 1500);
 });
-Promise.resolve(b).then((res) => {
-  console.log(11, res);
-});
-b.then((res, err) => {
-  console.log("我是结果", res, err);
-});
+// Promise.resolve(b).then((res) => {
+//   console.log(11, res);
+// });
+// b.then((res, err) => {
+//   console.log("我是结果", res, err);
+// });
 
 // ///////////严格模式下的函数参数是不会随arguments数组值改变而改变
+//////////////es6 proxy
+let arrayProxy = function (arr) {
+  let len = arr.length;
+  return new Proxy(arr, {
+    get(arr, index, qq) {
+      console.log("get", arr, index, qq);
+      if (index > 0) return arr[index];
+      let num = Math.abs(index);
+      return arr[len - num];
+    },
+  });
+};
+var a = arrayProxy([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(a[-1]);
